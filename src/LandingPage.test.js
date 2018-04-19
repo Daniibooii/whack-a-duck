9889edffe4ebe3e68ec8d7161b271cd0f89a7388
unknown <gw-danmace@hotmail.com>
2018-04-19 11:19:44 -0400

    changed App to LandingPage, edited it

--- src/LandingPage.test.js
+++ src/LandingPage.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import LandingPage from './LandingPage';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'; //need to change route for react
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import LandingPage from './LandingPage';
 import registerServiceWorker from './registerServiceWorker';
 //import Counter from "./Counter.js"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<LandingPage />, document.getElementById('root'));
 registerServiceWorker();
