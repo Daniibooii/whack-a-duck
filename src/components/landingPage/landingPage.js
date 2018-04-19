291254277e7622b41fddf4287d318cf74f1d4811
unknown <gw-danmace@hotmail.com>
2018-04-19 08:05:44 -0400

    added back in boilerplate for template js files; now renders; plugged in HTML colors for grass, water, sky

--- src/components/landingPage/landingPage.js
+++ src/components/landingPage/landingPage.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./LandingPage.css";
 
-/*const LandingPage = props => (
-<div className="">
- all the fun goes in here
+const LandingPage = props => (
+<div className="landing">
+   <h1>All the fun goes in here! </h1>
 </div>
-); */
+);
 
 
 export default LandingPage;
--- src/components/water/water.js
+++ src/components/water/water.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Water.css";
 
-/*const Water = props => (
-   <div className="">
-    all the fun goes in here
-  </div>
-); */
-
+const Water = props => (
+  <footer className="footer">
+    <div className="bottom">
+      <h1>All the fun goes in here! </h1>
+    </div>
+  </footer>
+);
 
 export default Water;
