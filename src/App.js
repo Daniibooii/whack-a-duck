8341308ecadcd3428d0978ed82af6436877e5c55
Daniel Mace <gw-danmace@hotmail.com>
2018-04-23 08:04:41 -0400

    code tidying/testing of socket.io

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LandingPage from "./components/LandingPage";
 /*import Pellets from "./components/Pellets";*/
 import Sidebar from "./components/Sidebar";
 import Water from "./components/Water";
-/*import "./App.css";*/
+import "./App.css";
 
 class App extends Component {
   render() {
