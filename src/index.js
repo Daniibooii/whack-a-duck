0c5bc4e329e1a7f196f10080a8404cd2ac43db2b
Daniel Mace <gw-danmace@hotmail.com>
2018-04-24 10:29:11 -0400

    more minor edits for Heroku compat

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import io from 'socket.io-client';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 //import Counter from "./Counter.js" ?
+
+// const socket = io();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
